824e33e815591c5cba61e0d8e8eba33864e4b45b
krldomingo <ksdomingo4@up.edu.ph>
2025-05-14 15:44:26 +0800

    feat: add logo to header and update favicon

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,25 @@
-<h1>
-    PASAKAY
-</h1>
+<div class="header">
+    <img src="logoIcon.PNG" alt="Logo" class="logoIcon">
+    <img src="logoText.PNG" alt="Pasakay" class="logoText">
+</div>
 
 <style>
-    h1 {
+    .header {
         background-color: #14141F;
         width: 100%;
         top:0px;
         left:0px;
         color: #B2C6CF;
-        text-align: center;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
         margin: 0px;
+        gap:0.3em;
+    }
+    .logoIcon {
+        height: 3em;
+    }
+    .logoText {
+        height: 1.5em;
     }
 </style>
